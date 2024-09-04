201bda77faa15bd3e947706b4bbf4b55d850b06a
Igor DevSec <radugasix@gmail.com>
2024-09-04 22:57:26 +0300

    Переделан по PHP для создания новых возможностей: 1. вынесение дублирующих записей в отдельный файл; 2. настройка обратной связи;

---
+++ about-full.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- CSS -->
+	<title>Бюджетное учреждение здравоохранения Орловской области "Орловская дезинфекционная станция"</title>
+</head>
+
+<body>
+
+	<!-- START HEADER -->
+	<?php include ('header.php'); ?>
+	<!-- END HEADER -->
+
+	<!-- START MAIN -->
+	<main class="main">
+		
+		<div class="about-full">
+			<div class="about-full__container container">
+
+				<h2 class="about-full__title">
+					О нас
+				</h2>
+
+				<div class="about-full__content">
+					<div class="about-full__text">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quos quod odio praesentium iste, ullam maiores harum voluptatum deleniti repellat asperiores ut incidunt obcaecati a quas iure, enim quia architecto earum. Nulla minus placeat dignissimos, explicabo quae aperiam. Itaque, vitae ipsa. Soluta in vero porro neque sed placeat quaerat at?</p>
+
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum doloremque quos, veniam eaque ullam qui maxime deserunt voluptas quidem quam nihil incidunt, tempora consequatur. Optio.</p>
+
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum doloremque quos, veniam eaque ullam qui maxime deserunt voluptas quidem quam nihil incidunt, tempora consequatur. Optio.</p>
+					</div>
+
+					<h3 class="about-full__title">
+						Команда
+					</h3>
+
+					<ul class="about-full__team-list">
+						<li class="about-full__team-item">
+							<img src="./img/PEOPLE/1.png" alt="Сотрудник">
+
+							<p class="about-full__team-item-name">
+								Иванов Иван
+							</p>
+
+							<p class="about-full__team-job">
+								Заместитель Директора
+							</p>
+						</li>
+
+						<li class="about-full__team-item">
+							<img src="./img/PEOPLE/1.png" alt="Сотрудник">
+
+							<p class="about-full__team-item-name">
+								Иванов Иван
+							</p>
+
+							<p class="about-full__team-job">
+								Заместитель Директора
+							</p>
+						</li>
+
+						<li class="about-full__team-item">
+							<img src="./img/PEOPLE/1.png" alt="Сотрудник">
+
+							<p class="about-full__team-item-name">
+								Иванов Иван
+							</p>
+
+							<p class="about-full__team-job">
+								Заместитель Директора
+							</p>
+						</li>
+
+						<li class="about-full__team-item">
+							<img src="./img/PEOPLE/1.png" alt="Сотрудник">
+
+							<p class="about-full__team-item-name">
+								Иванов Иван
+							</p>
+
+							<p class="about-full__team-job">
+								Заместитель Директора
+							</p>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+		</div>
+
+	</main>
+	<!-- END MAIN -->
+
+	<!-- START FOOTER -->
+	<!-- END FOOTER -->
+
+	<!-- START BURGER -->
+	<?php include ('burger.php'); ?>
+	<!-- END BURGER -->
+
+	<!-- START MODAL -->
+	<?php include ('modal.php'); ?>
+	<!-- END MODAL -->
+
+
+
+	<!-- JS -->
+	<script src="./script/main.js"></script>
+	<script src="./script/modal.js"></script>
+	<!-- JS -->
+
+</body>
+
+</html>
---
+++ form.php
@@ -0,0 +1,27 @@
+<form class="contact__form" action="send_mail.php" method="post">
+    <div class="contact__body">
+        <div class="contact__field field">
+            <label class="field__label" for="name">Имя</label>
+            <input class="field__input" id="name" name="name"
+                placeholder="Иванов Иван Иванович" />
+        </div>
+        <div class="contact__field field">
+            <label class="field__label" for="email">Почта*</label>
+            <input class="field__input" id="email" name="email" placeholder="test@test.ru"
+                type="email" required />
+        </div>
+        <div class="contact__field field">
+            <label class="field__label" for="phone">Телефон*</label>
+            <input class="field__input" id="phone" name="phone" placeholder="Телефон"
+                type="phone" required />
+        </div>
+        <!-- <div class="contact__field field">
+									<label class="field__label" for="message">Вопрос*</label>
+									<textarea class="field__input field__input--textarea" id="message" name="message"
+										placeholder="Хочется спросить про..." required></textarea>
+								</div> -->
+    </div>
+    <button class="contact__button button" type="submit">
+        Отправить
+    </button>
+</form>
